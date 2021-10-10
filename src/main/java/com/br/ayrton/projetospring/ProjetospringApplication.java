49d3d67109ac265a6c737d70f2115e6e3b95d6e5
Ayrton Barreto <devayrtonsbarreto@gmail.com>
2021-10-10 20:24:30 -0300

    Criando operação de instanciação de categorias ao iniciar a aplicação

--- src/main/java/com/br/ayrton/projetospring/ProjetospringApplication.java
+++ src/main/java/com/br/ayrton/projetospring/ProjetospringApplication.java
@@ -1,13 +1,31 @@
 package com.br.ayrton.projetospring;
 
+import com.br.ayrton.projetospring.domain.Categoria;
+import com.br.ayrton.projetospring.repositories.CategoriaRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetospringApplication {
+public class ProjetospringApplication implements CommandLineRunner {
+
+    private CategoriaRepository categoriaRepository;
+
+    public ProjetospringApplication(final CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetospringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
--- src/main/java/com/br/ayrton/projetospring/services/CategoriaService.java
+++ src/main/java/com/br/ayrton/projetospring/services/CategoriaService.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 @Service
 public class CategoriaService {
 
-
     private CategoriaRepository categoriaRepository;
 
     public CategoriaService (final CategoriaRepository categoriaRepository){
